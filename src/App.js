487af5acc867a73242ab6eeda97b17284139e751
--local <--local>
2024-08-13 09:22:55 +0530

    feat: google analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
 import "./App.css";
 import Animate from "./components/Animate/Animate";
 
 function App() {
+  useEffect(() => {
+    const analytics = document.createElement("script");
+    analytics.innerHTML = `
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6K8VLYHK37"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6K8VLYHK37');
+</script>
+    `;
+    document.body.appendChild(analytics);
+  }, []);
   return (
-    <div className="App" onContextMenu={(e)=>e.preventDefault()}>
-        
-    <Animate/>
+    <div className="App" onContextMenu={(e) => e.preventDefault()}>
+      <Animate />
     </div>
   );
 }
